9e76ce63a1c1868b3b06539c745778989f1c5d30
krzysiekmoro <moro.krzysiek@gmail.com>
2020-05-21 14:19:53 +0200

    personilized look for messages

--- client/src/components/MessageItem.js
+++ client/src/components/MessageItem.js
@@ -1,26 +1,28 @@
 import React from "react";
-import Moment from "react-moment";
 import { Link } from "react-router-dom";
+import DefaultImage from "../images/default-profile.png";
 
-const MessageItem = ({ date, text, username, profileImageUrl }) => (
-  <div>
+const MessageItem = ({
+  text,
+  username,
+  profileImageUrl,
+  justifyContent
+}) => (
+  <div className={`message-${justifyContent}`} >
     <img
-      src={profileImageUrl}
+      src={profileImageUrl || DefaultImage}
       alt={username}
-      height="100"
-      width="100"
-      className="timeline-image"
+      height="50"
+      width="50"
+      className="card-image"
     />
-    <div className="message-area">
-      <Link to="/">{username}&nbsp;</Link>
-      <span>
-        <Moment className="text-muted" format="DD MM YYYY">
-          {date}
-        </Moment>
-      </span>
-      <p>{text}</p>
+    <div className="card-content">
+      <Link id="card-username" to="/">{username}&nbsp;</Link>
+      <div className="card">
+        <p>{text}</p>
+      </div>
     </div>
   </div>
-);
+)
 
 export default MessageItem;
--- client/src/containers/MessageList.js
+++ client/src/containers/MessageList.js
@@ -8,23 +8,33 @@ class MessageList extends Component {
     this.props.fetchMessages();
   }
   render() {
-    const { messages } = this.props;
-    let messageList = messages.map((m) => (
-      <MessageItem
+    const { messages, currentUser } = this.props;
+    let messageList = messages.map((m) => {
+      let justifyContent = "start"
+      if(currentUser.user.id == m.user._id){
+        justifyContent = "end";
+      }
+      console.log(`Current user: ${currentUser.user.id}. 
+      Message user: ${m.user._id}`)
+      return(<MessageItem
         key={m._id}
         date={m.createdAt}
         text={m.text}
         username={m.user.username}
         profileImageUrl={m.user.profileImageUrl}
-      />
-    ));
-    return {messageList}
+        currentUser={currentUser}
+        user={m.user.id}
+        justifyContent={justifyContent}
+      />)
+    });
+    return messageList
   }
 }
 
 function mapStateToProps(state) {
   return {
     messages: state.messages,
+    currentUser: state.currentUser
   };
 }
 
